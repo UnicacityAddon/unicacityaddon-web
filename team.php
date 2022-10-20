feadd2d584a0b390c5d82443cef69cfee2d6d510
rettichlp <rettichlp01@gmail.com>
2022-10-20 23:59:50 +0200

    Exclude Developers because no difference to CEOs

--- team.php
+++ team.php
@@ -27,7 +27,7 @@
 
     $teams = array(
             0 => array(
-                    "name" => "CEO",
+                    "name" => "CEO & Developer",
                     "team" => $ceo,
                     "class" => "ceo"
             ),
@@ -57,6 +57,8 @@
         $teamName = $teamEntry["name"];
         $teamMember = $teamEntry["team"];
         $teamClass = $teamEntry["class"];
+
+        if ($teamName === "Developer") continue;
 
         echo "<p class='text-40-400' style='text-align: center'>" . $teamName . "</p>";
         echo "<div class='team-group'>";
