b8389de616a9cf3f1832aad7c5e808f9f9289749
rettichlp <rettichlp01@gmail.com>
2022-10-20 12:01:22 +0200

    Add php include files

---
+++ common/php/head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=chrome">
+<link rel="shortcut icon" href="/common/img/UCLogo.png" type="image/x-icon">
+<link rel="stylesheet" href="/common/css/master.css">
+<link rel="stylesheet" href="/common/css/font.css">
+<script src="/common/js/master.js"></script>
---
+++ common/php/nav.php
@@ -0,0 +1,15 @@
+<?php
+$api_url = 'http://rettichlp.de:8888/unicacityaddon/v1/dhgpsklnag2354668ec1d905xcv34d9bdee4b877/mgmt';
+$json_data = json_decode(file_get_contents($api_url));
+$latestVersion = $json_data->latestVersion;
+?>
+<nav id="nav" class="nav">
+    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/releases/download/v<?php echo $latestVersion ?>/UnicacityAddon-<?php echo $latestVersion ?>.jar">Download</a>
+    <span class="nav-child text-20-400" style="float: right"> | </span>
+    <a class="nav-child nav-child-right text-20-400" href="/houseBans.php">House Bans</a>
+    <a class="nav-child nav-child-right text-20-400" href="/wantedReasons.php">Wanted Reasons</a>
+    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/wiki">Wiki</a>
+    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/releases/latest">What's new?</a>
+    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/tree/main">Code</a>
+    <a class="nav-child nav-child-left text-20-800" href="/index.php">UnicacityAddon</a>
+</nav>
