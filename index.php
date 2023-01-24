0e862c244e2810563aebdd8fa85fe85b18b7e766
rettichlp <rettichlp01@gmail.com>
2023-01-24 14:13:17 +0100

    Add smooth scroll and arrows on index page

--- index.php
+++ index.php
@@ -75,7 +75,17 @@ $versions = $json_data->versions;
     </div>
 </div>
 
-<div class="index-statistic">
+<a href="#index-statistic">
+    <div class="arrow">
+        <svg>
+            <polygon points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
+            <polygon points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
+            <polygon points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
+        </svg>
+    </div>
+</a>
+
+<div id="index-statistic" class="index-statistic">
     <div class="index-statistic-grid">
         <div class="grid-a grid-center">
             <h2>Statistiken</h2>
