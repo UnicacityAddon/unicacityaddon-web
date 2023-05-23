0a8b03786db1a9bd12ea3a7725bac617b70c62ad
rettichlp <rettichlp01@gmail.com>
2023-05-23 15:18:22 +0200

    Update wiki links and organisation repository link

--- common/php/nav.php
+++ common/php/nav.php
@@ -3,14 +3,14 @@ $json_data = json_decode(file_get_contents('http://rettichlp.de:8888/unicacityad
 $latestVersion = $json_data->latestVersion;
 ?>
 <nav id="nav" class="nav">
-    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/releases/download/v<?php echo $latestVersion ?>/UnicacityAddon-<?php echo $latestVersion ?>.jar">Download</a>
+    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/unicacityaddon-addon/releases/download/v<?php echo $latestVersion ?>/UnicacityAddon-<?php echo $latestVersion ?>.jar">Download</a>
     <span class="nav-child text-20-400" style="float: right"> | </span>
     <a class="nav-child nav-child-right text-20-400" href="/banner">Banner</a>
     <a class="nav-child nav-child-right text-20-400" href="/houseBans">House Bans</a>
     <a class="nav-child nav-child-right text-20-400" href="/wantedReasons">Wanted Reasons</a>
     <a class="nav-child nav-child-right text-20-400" href="/team">Team</a>
-    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/wiki">Wiki</a>
-    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/releases/latest">What's new?</a>
-    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/UnicacityAddon-1.12.2/tree/main">Code</a>
+    <a class="nav-child nav-child-right text-20-400" href="https://wiki.unicacityaddon.rettichlp.de/">Wiki</a>
+    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/unicacityaddon-addon/releases/latest">What's new?</a>
+    <a class="nav-child nav-child-right text-20-400" href="https://github.com/rettichlp/unicacityaddon-addon/tree/main">Code</a>
     <a class="nav-child nav-child-left text-20-800" href="/">UnicacityAddon</a>
 </nav>
